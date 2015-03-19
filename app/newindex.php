aa0559f3206df9c5893907bb7e6e3b2a250d27ea
Thomas Gaubert <tpgaubert@gmail.com>
2015-03-19 17:49:14 -0500

    Start new player mockup... added missing error handling

---
+++ app/newindex.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Ensemble</title>
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="res/css/newplayer.css">
+		<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,300italic,400italic' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<div class="top-bar">
+			<div class="row">
+			    <div class="col-xs-6">
+			    	<div class="row">
+			    		<div class="col-xs-3">
+			    			<a class="nav-item active">Home</a>
+			    		</div>
+			    		<div class="col-xs-3">
+			    			<a class="nav-item">Music</a>
+			    		</div>
+			    		<div class="col-xs-3">
+			    			<a class="nav-item">Settings</a>
+			    		</div>
+			    	</div>
+			    </div>
+			    <div class="col-xs-6">
+			    	<div class="logo pull-right">Ensemble</div>
+			    </div>
+			</div>
+		</div>
+	</body>
+</html>
