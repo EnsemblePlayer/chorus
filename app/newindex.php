4127a4ab4f7b4388db46e34e3c7c0ef900feca95
Thomas Gaubert <tpgaubert@gmail.com>
2015-04-02 14:59:05 -0500

    ...just kidding...

--- app/newindex.php
+++ app/newindex.php
@@ -23,6 +23,7 @@
 		<link rel="stylesheet" href="res/css/reset.css"> <!-- CSS reset -->
 		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 		<link rel="stylesheet" href="res/css/newplayer.css"> <!-- Resource style -->
+		<link rel="stylesheet" href="res/css/newplayermodal.css">
 		<link rel="stylesheet" href="res/css/table.css">
 		<script src="res/js/modernizr.js"></script> <!-- Modernizr -->
 	  	
