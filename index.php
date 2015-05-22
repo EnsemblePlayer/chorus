53dfa70e2d53baab81ab7949680fb8182882c2c0
TexasGamer <tpgaubert@gmail.com>
2015-05-21 20:48:37 -0500

    Clean up landing header
    Better looking on mobile devices

--- index.php
+++ index.php
@@ -22,58 +22,51 @@
 	
 	<?php include "api/includes/dev.php" ?>
 	<body style="font-family: 'Roboto', sans-serif; background: #333; color: white;">
-		<div id="fullpage">
-			<div class="section">
-				<div style="width: 100%; height: 100%;">
-					<div style="background-size: cover; background-image: url('res/img/albums.jpg'); height: 400px; padding: 25px; box-shadow: 0px 400px rgba(73, 73, 73, 0.6) inset;">
-						<div style="padding-top: 15px; text-align: center;">
-							<p><span style="font-family: 'Lobster', cursive; color: #f2f2f2; font-size: 128px;">Ensemble</span></p>
-							<p><span style="color: #f2f2f2; font-size: 28px; font-weight: 300;">unified streaming music</span></p>
-							<div class="buttons">
-								<a href="app/register.php" class="button download">Register</a>
-								<a href="app/login.php" class="button github">Login</a> 
-								<!-- &nbsp;
-								<a href="" class="button register">Register</a> &nbsp;
-								<a href="" class="button login">Login</a> -->
-							</div>
-						</div>
+		<div class="banner">
+			<div class="banner-bg">
+				<div class="banner-text">
+					<p><span class="banner-title">Ensemble</span></p>
+					<p><span class="banner-subtitle">unified streaming music</span></p>
+					<div class="buttons">
+						<a href="app/register.php" class="button register">Register</a>
+						<a href="app/login.php" class="button login">Login</a>
 					</div>
 				</div>
 			</div>
+		</div>
+		
+		<div class="container" style="text-align: center;">
+			<h1>Your Music</h1>
+			<p>Login to your music streaming services of choice and listen away.<p>
 			
-			<div class="container" style="text-align: center;">
-				<h1>Your Music</h1>
-				<p>Login to your music streaming services of choice and listen away.<p>
-				
-				<img src="res/img/playmusic.png" width="150" height="150" style="margin-right: 12px;"/> <i class="fa fa-plus fa-2x"></i>
-				<img src="res/img/youtube.png" width="150" height="106" style="margin-left: 12px; margin-right: 12px;"/> <i class="fa fa-plus fa-2x"></i>
-				<img src="res/img/spotify.png" width="150" height="150" style="margin-left: 12px;"/>
-				
-				<h1>Your Friends</h1>
-				<p>Create and manage a queue made up of songs from different people and services.</p>
-				<img src="res/img/list.png" width="150" height="150"/>
-				
-				<h1>Your Device</h1>
-				<p>
-					Designed with cross platform compatibility and performance in mind,<br>instantly create a music streamer wherever you are.
-				</p>
-				<img src="res/img/osx.png" width="100" height="100"/>
-				<img src="res/img/pi.png" width="150" height="150"/>
-				<img src="res/img/win.png" width="100" height="100"/>
-			</div>
+			<img src="res/img/playmusic.png" width="150" height="150" style="margin-right: 12px;"/> <i class="fa fa-plus fa-2x"></i>
+			<img src="res/img/youtube.png" width="150" height="106" style="margin-left: 12px; margin-right: 12px;"/> <i class="fa fa-plus fa-2x"></i>
+			<img src="res/img/spotify.png" width="150" height="150" style="margin-left: 12px;"/>
 			
-			<!-- FOOTER -->
-			<div style="bottom: 0; width: 100%; text-align: center; margin-top: 100px;">
-				<span>
-					<i class="fa fa-code"></i> with <i class="fa fa-heart"></i> by Anthony Bauer and Thomas Gaubert and Grant Uy<br>
-					HackDFW 2015 - The University of Texas at Austin</p>
-				</span>
-				<br>
-				<a href="http://github.com/EnsemblePlayer" class="button github">
-					<i class="fa fa-github" style="padding-top: 6px;"></i> GitHub
-				</a>
-				<img src="res/img/buildings.png" width="100%"/>
-			</div>
+			<h1>Your Friends</h1>
+			<p>Create and manage a queue made up of songs from different people and services.</p>
+			<img src="res/img/list.png" width="150" height="150"/>
+			
+			<h1>Your Device</h1>
+			<p>
+				Designed with cross platform compatibility and performance in mind,<br>instantly create a music streamer wherever you are.
+			</p>
+			<img src="res/img/osx.png" width="100" height="100"/>
+			<img src="res/img/pi.png" width="150" height="150"/>
+			<img src="res/img/win.png" width="100" height="100"/>
+		</div>
+		
+		<!-- FOOTER -->
+		<div style="bottom: 0; width: 100%; text-align: center; margin-top: 100px;">
+			<span>
+				<i class="fa fa-code"></i> with <i class="fa fa-heart"></i> by Anthony Bauer and Thomas Gaubert and Grant Uy<br>
+				HackDFW 2015 - The University of Texas at Austin</p>
+			</span>
+			<br>
+			<a href="http://github.com/EnsemblePlayer" class="button github">
+				<i class="fa fa-github" style="padding-top: 6px;"></i> GitHub
+			</a>
+			<img src="res/img/buildings.png" width="100%"/>
 		</div>
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
